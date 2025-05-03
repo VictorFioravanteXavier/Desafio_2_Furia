2bce489c28415f2ccc8b5ecc70a51e66c59299cb
VictorFioravanteXavier <victor.fx0910@gmail.com>
2025-05-03 11:41:42 -0300

    style(nav-bar): aterando o style e adicionando outras funcionalidades

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
